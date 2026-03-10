e51a700bcb7784879336963bd5c50b157ced9e30
BRAYAN ALVES DA SILVA CLARA <25203616@CFP127ALUNO.LOCAL>
2026-03-10 11:09:44 -0300

    exercicio 1 concluido

---
+++ 01-teste-aptidao/app.js
@@ -0,0 +1,24 @@
+"use strict"
+
+
+
+
+function avaliar(){
+    const tempo = document.getElementById("tempo")
+    const resultado = document.getElementById("resultado")
+
+    removerClasses()
+    if(tempo.value < 14){
+        resultado.textContent = "Apto"
+        resultado.classList.add("apto")
+    }else{
+        resultado.textContent = "Não apto"
+       
+        resultado.classList.add("nao-apto")
+
+    }
+}
+function removerClasses (){ 
+    const resultado = document.getElementById("resultado")
+    resultado.classList.remove('apto', 'nao-apto')
+}
